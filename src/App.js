ae674cf0528424c8137fb8c058d8777ca4e6919d
2331sakshi <skshpanchal@gmail.com>
2022-06-30 19:35:17 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Homepage from './Pages/Homepage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SingleBlog from './Pages/SingleBlog';
@@ -10,8 +10,7 @@ function App() {
   const GoToSingleBlog = ({match}) => {
     return(
 
-      // PARSEINT IS A JS FUNCTION WHICH WILL CONVERT THE THE GIVEN STRING OF NUMBERS 
-      // ACCORDING TO THE BASE OR RADIX SPECIFIED
+      // PARSEINT IS A JS FUNCTION WHICH WILL CONVERT THE THE GIVEN STRING OF NUMBERS ACCORDING TO THE BASE OR RADIX SPECIFIED
       <SingleBlog id={match.params.id} />
     );
   }
@@ -19,10 +18,10 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Switch>
+      <Routes>
         <Route exact path="/" component={Homepage} />
         <Route exact path="/blog/:id" component={GoToSingleBlog} />
-      </Switch>
+      </Routes>
       </BrowserRouter>
     </div>
   );
